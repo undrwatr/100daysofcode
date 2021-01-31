06a1f2735923a566d374f8a2b8b41613a727997c
Joseph Jenkins <josephmjenkins@me.com>
2021-01-31 07:40:05 -0800

    Day 1 coding program

---
+++ day1-band-name/band-name.py
@@ -0,0 +1,13 @@
+#script to generate your band name based on city and pet's name
+print("This will generate your brand name")
+
+#ask for the city
+
+city = input("What city did you grow up in?\n")
+
+# ask for pet's name
+pet = input("What is the name of your pet?\n")
+
+# combine them together and print them out
+
+print("Your Band's name is " + city + ' ' + pet)
